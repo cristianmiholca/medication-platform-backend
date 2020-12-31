368f265f409e1b8edc2a90fdfaac33034f27ce1c
Cristian Miholca <cristimiholca@gmail.com>
2020-12-31 20:57:42 +0200

    Fixed JsonRpcServiceImpl

--- src/main/java/com/utcn/medicationplatform/jsonrpc/JsonRpcServiceImpl.java
+++ src/main/java/com/utcn/medicationplatform/jsonrpc/JsonRpcServiceImpl.java
@@ -9,7 +9,6 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import javax.rmi.CORBA.Util;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
